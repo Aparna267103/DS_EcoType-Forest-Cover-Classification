0fbe8e91b459bc5c054b120a27686ea479d2db4c
Aparna V <aparnavellingiri267@gmail.com>
2026-08-13 14:03:45 +0530

    Refactor EDA.py comments and sample function
    Updated comments for clarity and added random_state to sample.

--- EDA.py
+++ EDA.py
@@ -27,9 +27,9 @@ print(df.duplicated().sum())
 
 
 # ================== TARGET DISTRIBUTION ==================
-plt.figure(figsize=(8, 5))
+plt.figure(figsize=(8, 5))      # Check 7 cover type class balances
 
-sns.countplot(x='Cover_Type', data=df)
+sns.countplot(x='Cover_Type', data=df)  
 
 plt.title("Cover Type Distribution")
 
@@ -39,7 +39,7 @@ plt.show()
 # ================== HISTOGRAM ==================
 plt.figure(figsize=(8, 5))
 
-sns.histplot(df['Elevation'], kde=True)
+sns.histplot(df['Elevation'], kde=True)  # Histoplot -distribution is skewed.
 
 plt.title("Elevation Distribution")
 
@@ -47,9 +47,9 @@ plt.show()
 
 
 # ================== BOXPLOT ==================
-plt.figure(figsize=(8, 5))
+plt.figure(figsize=(8, 5))      # Check the outliers
 
-sns.boxplot(x=df['Slope'])
+sns.boxplot(x=df['Slope'])      
 
 plt.title("Slope Boxplot")
 
@@ -57,12 +57,12 @@ plt.show()
 
 
 # ================== CORRELATION HEATMAP ==================
-plt.figure(figsize=(14, 10))
+plt.figure(figsize=(14, 10))    # correlation calculation between numerical columns
 
 # Select only numeric columns
 numeric_df = df.select_dtypes(include=['number'])
 
-sns.heatmap(
+sns.heatmap(                 
     numeric_df.corr(),
     cmap='coolwarm'
 )
@@ -74,8 +74,8 @@ plt.show()
 # ================== SCATTERPLOT ==================
 plt.figure(figsize=(8, 5))
 
-sns.scatterplot(
-    x='Elevation',
+sns.scatterplot(        # bivariate analysis-Visually see how two features together separate Cover Type. 
+    x='Elevation',      
     y='Slope',
     hue='Cover_Type',
     data=df
@@ -87,8 +87,8 @@ plt.show()
 
 
 # ================== PAIRPLOT ================== 
-sample_df = df.sample(500) 
+sample_df = df.sample(500, random_state=42)
 
-sns.pairplot( sample_df, hue='Cover_Type' ) 
+sns.pairplot( sample_df, hue='Cover_Type' )  # Multiple feature compare in sametime
 
 plt.show()
